8dd27e22e7206d18a6a1a1dae91d9ff6d56b8ae1
nkligang <nkligang@163.com>
2014-08-26 20:15:39 +0800

    add clean file
    add clean file

---
+++ clean.bat
@@ -0,0 +1,6 @@
+cd .
+rmdir /s/q .\starling\bin-debug
+rmdir /s/q .\samples\demo_mobile\bin-debug
+rmdir /s/q .\samples\demo_web\bin-debug
+rmdir /s/q .\samples\scaffold_mobile\bin-debug
+rmdir /s/q .\tests\bin-debug
